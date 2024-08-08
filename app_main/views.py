30c2ae3b8262d73bcb096afbb9283c263751ba19
Craig Allen <callen7@hotmail.co.uk>
2024-08-08 13:50:40 +0000

    Add class TaskUpdate to views.py

--- app_main/views.py
+++ app_main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -17,6 +17,11 @@ class TaskDetail(DetailView):
     template_name = 'base/task.html'
 
 class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
