d2d02f3febf082f64300b28422d782d639d38354
Craig Allen <callen7@hotmail.co.uk>
2024-08-08 14:17:24 +0000

    Add LoginRequiredMixin to views.py

--- app_main/views.py
+++ app_main/views.py
@@ -4,19 +4,20 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
 
 # Create your views here.
 
 class CustomLoginView(LoginView):
-    template_name = 'base/login.html'
+    template_name = 'app_main/login.html'
     fields = '__all__'
     redirect_authenticated_user = True
 
     def get_success_url(self):
         return reverse_lazy('tasks')
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
