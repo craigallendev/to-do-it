118da0fffb1070fda35ca0cd3cd25b2e3e876727
Craig Allen <callen7@hotmail.co.uk>
2024-08-08 13:33:18 +0000

    Add basic html to task_list.html to show items

--- to_do_it/settings.py
+++ to_do_it/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'to_do_it.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [BASE_DIR / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
