beaaff8a68034c8034a335f3743f6f545b03ec08
Craig Allen <callen7@hotmail.co.uk>
2024-08-08 14:27:41 +0000

    Add FormView

--- app_main/views.py
+++ app_main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Task
 
 
